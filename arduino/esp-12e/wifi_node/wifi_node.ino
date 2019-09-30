e0c88b2ed2e1f4950eb70211cf2a9c76f6fb2a48
Manu <bodynodes.dev@gmail.com>
2019-09-30 22:42:17 +0100

    Adding Arduino ESP-12E Wifi Node implementation

---
+++ arduino/esp-12e/wifi_node/wifi_node.ino
@@ -0,0 +1,62 @@
+/**
+* MIT License
+* 
+* Copyright (c) 2019 Manuel Bottini
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#include "basics.h"
+#include "sensor.h"
+#include "wifi_conn.h"
+#include "actuators.h"
+
+
+void initStatusHMI(){
+  initStatusSensorHMI();
+  initStatusConnectionHMI();
+}
+
+void setup() {  
+  //Initialize serial and wait for port to open:
+  Serial.begin(921600);
+  setWifiNotConnected();
+
+  initActuators();
+  initStatusHMI();
+  initSensor();
+  initWifi();
+}
+
+void loop() {
+  if(!checkSensorInit()){
+    return;
+  }  
+  if(!isCalibrationOK()){
+  }
+  checkWifiAndServer();
+  if(isServerConnected()){
+    if(checkReadFromSensor()){
+      sendOrientationValueQuat(getReadQuat());
+    }
+    Action action = checkActionWifi();
+    setAction(action);
+  }
+  makeActions();
+}
